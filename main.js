bc499c120da3d54a7da9e187c9b83679a21a26a3
fernando duenas <fernando39615@aim.com>
2020-10-08 10:30:56 -0500

    commit

--- main.js
+++ main.js
@@ -116,17 +116,18 @@ for(let i = 3; i < sliceArr.length; i++){
 // Challenge 12
 // Pull the items at indices 4-6 (inclusive) from the following array using .splice (NOT .slice!), then console.log out each item from the ORIGINAL array. Note that .splice DOES change the original array.
 const dinosaurs = ['Velociraptor', 'T-Rex', 'Stegosaurus', 'Triceratops', 'Dimetrodon', 'Allosaur', 'Spinosaurus', 'Gigantosaur']
-dinosaurs.splice(4,6);
+dinosaurs.splice(4,3);
 console.log(dinosaurs);
 
 
 
 // Challenge 13
 // Use .join to change the dinosaur strings from the above array into one string with a '*' string as the "separator". Then console.log out that string.
-
+console.log(dinosaurs.join('*'));
 
 // Challenge 14
 // Use .reverse to reverse the dinosaur array and then log out the original array. .reverse DOES change the original array.
+console.log(dinosaurs.reverse());
 
 
 // Challenge 15
