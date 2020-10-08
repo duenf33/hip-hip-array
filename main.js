226c11a2b763029f5daa986f7144d6bf41dc5198
fernando duenas <fernando39615@aim.com>
2020-10-08 08:13:48 -0500

    first commit

--- main.js
+++ main.js
@@ -96,17 +96,29 @@ for(let i = 3; i < students.length; i++){
 
 // Challenge 10
 // Loop through the `students` array from Challenge 1, making a COPY of the array, starting at index 3 and ending at index 10 (INCLUSIVE). This is very similar to string building; create a new array, loop through the old one, and put values from the original in the new one. Then log out each value of the copy individually.
+let newArr = [];
 for(let i = 3; i < students.length; i++){
+  newArr[i] = students[i];
   console.log(students[i]);
 }
+console.log(newArr);
 
 // Challenge 11
 // Make a COPY of the `students` array *using .slice*, starting at index 3 and ending at index 10 (INCLUSIVE), then log out each value of the copy individually. THEN log out each value of the original array. Notice that the original array didn't get any values taken out of it, because `slice` does NOT modify the original array.
-
+console.log(students);
+const sliceArr = students.slice(3,10);
+console.log(sliceArr);
+for(let i = 3; i < sliceArr.length; i++){
+  console.log(sliceArr[i]);
+  console.log(students[i]);
+}
 
 // Challenge 12
 // Pull the items at indices 4-6 (inclusive) from the following array using .splice (NOT .slice!), then console.log out each item from the ORIGINAL array. Note that .splice DOES change the original array.
 const dinosaurs = ['Velociraptor', 'T-Rex', 'Stegosaurus', 'Triceratops', 'Dimetrodon', 'Allosaur', 'Spinosaurus', 'Gigantosaur']
+dinosaurs.splice(6, 4);
+console.log(dinosaurs);
+
 
 
 // Challenge 13
